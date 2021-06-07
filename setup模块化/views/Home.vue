1650bc20cc11dfab8e396681d15d955be9e3d115
zhaozhou <zhou773@foxmail.com>
2021-06-07 20:15:28 +0800

    feat: 模块化01

---
+++ setup模块化/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <p>x: {{x}}</p>
+    <p>y: {{y}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs, computed, ref,
+    watch,
+    onMounted,
+    onUnmounted
+} from 'vue';
+import mouse from "./useMosePosition"
+
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+
+  },
+  setup: function () {
+    const {x, y} = mouse();
+    // const data = reactive({
+    //   ...m1
+    // })
+    return {
+      x, y
+    }
+  }
+});
+</script>
---
+++ setup模块化/views/useMosePosition.ts
@@ -0,0 +1,54 @@
+import {onMounted, onUnmounted, reactive, toRefs, ref} from "vue";
+
+
+interface mousePositions {
+    x: number;
+    y: number;
+    setXY:(e: MouseEvent) => any;
+}
+
+
+function mouse () {
+    // const x = ref(0);
+    // const y = ref(0);
+    //
+    // const update = (e:MouseEvent) => {
+    //     x.value = e.pageX;
+    //     y.value = e.pageY
+    // }
+    // onMounted(() => {
+    //     document.addEventListener('click', update)
+    // })
+    //
+    // onUnmounted(() => {
+    //     document.removeEventListener('click', update)
+    // })
+    // return { x, y }
+
+    const data = reactive({
+        x: 0,
+        y: 0,
+    });
+    const  setData = (e: MouseEvent) => {
+        data.x = e.pageX;
+        data.y = e.pageY;
+        console.error(data);
+    }
+
+    onMounted(() => {
+        document.addEventListener('click', setData);
+    })
+
+    onUnmounted(() => {
+        document.removeEventListener('click', setData);
+    })
+
+
+    const reactData = toRefs(data)
+
+    return {
+        ...reactData
+    }
+}
+
+export default mouse;
